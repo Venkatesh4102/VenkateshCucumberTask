0b1ae2e77185dfe30aa9a79cbac347ef9425acc1
Venkatesh4021 <r.k.venkateshvales@gmail.com>
2019-09-10 18:19:16 +0530

    update

--- src/test/java/org/test/TestRunner.java
+++ src/test/java/org/test/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\resources\\feature\\scenerio.feature",glue="org.test",plugin="html:target")
+@CucumberOptions(features="src\\test\\resources\\feature\\scenerio.feature",glue="org.test",plugin= {"json:target/ven.json"})
 public class TestRunner {
 
 }
